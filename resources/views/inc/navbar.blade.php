21a133977ed64903656df99565e4e771d8e0a4a2
Melissa <melissarenaut@gmail.com>
2017-11-28 21:25:28 -0500

    fixing navbar height

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,7 +1,7 @@
 <!--old navbar, not being used currently-->
 
 <div id="navbarContainer">
-	<nav class="navbar navbar-default navbar-fixed-top" data-spy="affix" data-offset-top="197">
+	<nav class="navbar navbar-default navbar-fixed-top" data-spy="affix" data-offset-top="197" id="header">
 
 		<div class="container">
 
