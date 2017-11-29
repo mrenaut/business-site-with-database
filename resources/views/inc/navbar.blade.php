fcc0bb536bf0e44d7ddc56bd63f4371dc36c246d
Melissa <melissarenaut@gmail.com>
2017-11-29 02:34:16 -0500

    added contacts database to navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -54,7 +54,7 @@
 								<a href="{{ route('logout') }}" onclick="event.preventDefault();
 											  document.getElementById('logout-form').submit();">Logout
 								</a>
-                                        <a href="/contacts">Contact Database</a>
+                                       
 
 								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 									{{ csrf_field() }}
