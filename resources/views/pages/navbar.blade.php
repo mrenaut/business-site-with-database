f56d6631b6111d110cfd8b5fa91ec2a7576682e8
Melissa <melissarenaut@gmail.com>
2017-11-28 21:59:03 -0500

    fixing navbar height

--- resources/views/pages/navbar.blade.php
+++ resources/views/pages/navbar.blade.php
@@ -20,7 +20,7 @@
 					<a class="navbar-brand" rel="home" href="#">
 						<img style="max-width:10px; margin-top: -7px;" src="/assets/images/logo.png">
 					</a>
-					<a class="navbar-brand" href="/">PROTEC INSURANCE</a>
+					<a class="navbar-brand" href="/">yo mama INSURANCE</a>
 				</div>
 
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
